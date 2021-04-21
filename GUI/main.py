d4ea65a7fadf03b3cee8e0859c09dfafadce7473
rztumbleson <rztumbleson@gmail.com>
2021-04-21 12:41:28 -0700

    Better camera handling

--- GUI/main.py
+++ GUI/main.py
@@ -2,12 +2,10 @@
 import PyQt5
 import numpy as np
 import glob
-import os
 
-from mumax_helper_func import *
 from traits.api import HasTraits, Range, String, Instance, Button, \
     on_trait_change, Property, cached_property
-from traitsui.api import View, Item, Group, HSplit
+from traitsui.api import View, Item, Group, HSplit, VSplit, DefaultOverride
 
 from mayavi import mlab
 from mayavi.core.api import PipelineBase, Source
@@ -31,8 +29,11 @@ class VectorCuts(HasTraits):
     Y = Range(0, 'dim_y', 'dim_y/2', mode='slider')
     Z = Range(0, 'dim_z', 'dim_z/2', mode='slider')
     t = Range(0, 'time_steps', 0, mode='slider')
-    camX = Range(-1000, 'dim_x', 'dim_x/2', mode='slider')
-    camY = Range(-1000, 'dim_y', 'dim_y/2', mode='slider')
+    cam_slider = Range(1, 20, 1, mode='slider')
+    camX_button_pos = Button(label='+X')
+    camX_button_neg = Button(label='-X')
+    camY_button_pos = Button(label='+Y')
+    camY_button_neg = Button(label='-Y')
     resetCam = Button(label='Reset Camera')
 
     # modules
@@ -86,15 +87,17 @@ class VectorCuts(HasTraits):
     def display_scene(self):
         self.make_all_plots_nice()
 
-    @on_trait_change('X, Y, Z, camX, camY, camZ')
+    @on_trait_change('X, Y, Z')
     def update_plot(self):
         self.scene.disable_render = True
-        self.plotx.implicit_plane.plane.origin = (self.X, self.camY, 0)
-        self.ploty.implicit_plane.plane.origin = (self.camX, self.Y, 0)
-        self.plotz.implicit_plane.plane.origin = (self.camX, self.camY, self.Z)
-
-        self.scene.mlab.view(focalpoint=(self.camX, self.camY, 0))
+        self.plotx.implicit_plane.plane.origin = (self.X, 0, 0)
+        self.ploty.implicit_plane.plane.origin = (0, self.Y, 0)
+        self.plotz.implicit_plane.plane.origin = (0, 0, self.Z)
         self.scene.disable_render = False
+
+    @on_trait_change('camX, camY')
+    def update_cam(self):
+        self.scene.mlab.move(0, self.camX, self.camY)
 
     @on_trait_change('t')
     def update_time(self):
@@ -106,9 +109,23 @@ class VectorCuts(HasTraits):
 
     @on_trait_change('resetCam')
     def reset_camera(self):
-        self.scene.mlab.view(focalpoint=(256, 256, 0))
-        self.camX = 256
-        self.camY = 256
+        self.scene.mlab.view(focalpoint=(self.dim_x/2, self.dim_y/2, 0))
+
+    @on_trait_change('camX_button_neg')
+    def button_x_neg(self):
+        self.scene.mlab.move(0, self.cam_slider, 0)
+
+    @on_trait_change('camX_button_pos')
+    def button_x_pos(self):
+        self.scene.mlab.move(0, -1 * self.cam_slider, 0)
+
+    @on_trait_change('camY_button_neg')
+    def button_y_neg(self):
+        self.scene.mlab.move(0, 0, self.cam_slider)
+
+    @on_trait_change('camY_button_pos')
+    def button_y_pos(self):
+        self.scene.mlab.move(0, 0, -1 * self.cam_slider)
 
     def make_all_plots_nice(self):
         # speed up code slightly
@@ -127,30 +144,37 @@ class VectorCuts(HasTraits):
         self.scene.scene.background = (0, 0, 0)
 
         # set the default camera view
-        self.scene.mlab.view(focalpoint=(self.camX, self.camY, 0))
+        self.scene.mlab.view(focalpoint=(self.dim_x/2, self.dim_y/2, 0))
         self.scene.mlab.orientation_axes()
 
     view = View(HSplit(
         Group(
             Item('scene', editor=SceneEditor(scene_class=MayaviScene),
                  height=700, width=1200, show_label=False)),
-        Group(
-            Item('X'),
-            Item('Y'),
-            Item('Z'),
-            Item('t'),
-            Item('camX'),
-            Item('camY'),
-            Item('resetCam', show_label=False)
+        Group(VSplit(
+            Group(
+                Item('X'),
+                Item('Y'),
+                Item('Z'),
+                Item('t'),
+                Item('_'),
+                Item('resetCam', show_label=False, full_size=True),
+                Item('camY_button_pos', show_label=False),
+                Item('camY_button_neg', show_label=False),
+                Item('camX_button_pos', show_label=False),
+                Item('camX_button_neg', show_label=False),
+                Item('cam_slider', show_label=False, editor=DefaultOverride(low_label='fine', high_label='coarse'))
+            ),
+            )
+        )
         ),
-    ),
         resizable=True,
         title='Vector Field Cuts'
     )
 
 
 if __name__ == '__main__':
-    ETS_TOOLKIT = PyQt5
-    path = 'G:\\My Drive\\Mumax\\FeGd\\5pcX_300K-3nsRelax.out\\'
+    path = 'G:\\My Drive\\Mumax\\Steps\\steps10Oe.out\\' #100,000kB
+    #path = 'G:\\My Drive\\Mumax\\FeGd\\5pcX_300K-5nsRelax60nmThick.out\\' #12,000kB
     vc = VectorCuts(path=path)
     vc.configure_traits()
